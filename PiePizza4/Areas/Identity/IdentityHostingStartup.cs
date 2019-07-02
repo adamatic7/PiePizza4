7a57b6aa7addfb5bfb2c42cda645f0f384d1d4d1
Adam Miranda <adamballimiranda@mail.weber.edu>
2019-07-01 22:45:42 -0600

    Add project files.

---
+++ PiePizza4/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PiePizza4.Data;
+
+[assembly: HostingStartup(typeof(PiePizza4.Areas.Identity.IdentityHostingStartup))]
+namespace PiePizza4.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
